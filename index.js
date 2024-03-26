1f3ed3f7ef40f99ca102745b8be792f4db675f76
amososwom <amososwom162@gmail.com>
2024-03-26 21:32:29 +0300

    Array-Find Completed

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const record = [
     { year: "2013", result: "L"},
   ]
 function superbowlWin(recordArray){
-    let response = ""; // 
-    response = recordArray.find( object => (object.result === "W")); //
-    response = (response === undefined) ? undefined : response.year; //
+    let response = ""; // we declare an empty array here 
+    response = recordArray.find( object => (object.result === "W")); // check for the result W in the array and return that object or undefined
+    response = (response === undefined) ? undefined : response.year; // cheks the response and return the year if it is not undefine,
  
     return response;
 }
